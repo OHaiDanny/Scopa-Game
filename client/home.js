ac520e46c543258ae14c5c30f928a0c0c7790fd9
Danny Fang Yan <dannyfyan@gmail.com>
2015-10-21 14:49:43 -0700

    Add extra user logic
    - added so that users can’t play against themselves

--- client/home.js
+++ client/home.js
@@ -1,5 +1,20 @@
 Template.userList.helpers({
    users:function() {
-       return Meteor.users.find();
+       var myId = Meteor.userId(),
+           cantPlayAgainst = [myId];
+       
+       Games.find({inProgress: true }).forEach(function (game) { // find all games that current user is playing
+           cantPlayAgainst.push(game.currentTurn[game.currentTurn[0] === myId ? 1 : 0]); // find who they are playing against and pushing it to the can't play against
+       });
+       
+       
+       // find users that their id is not in can't play against
+       return Meteor.users.find({ _id: {$not: {$in: cantPlayAgainst}}});
+   } 
+});
+
+Template.userItem.events({
+   'click button': function(evt, template) {
+       
    } 
 });
